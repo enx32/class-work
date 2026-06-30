3de842ad374e4c5e5746887b212e97b116c9e2af
enx <enx2134@gmail.com>
2026-06-30 15:20:38 +0300

    feat: add project files

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,47 +1,84 @@
 ﻿#include <iostream>
 
-void takeDamage(int* hp, int damage);
-void heal(int* hp, int amount);
-void printHp(const int* hp);
-bool isAlive(const int* hp);
-
-void takeDamage(int* hp, int damage) {
-    if (*hp > 0) {
-        *hp -= damage;
-    }
-}
-void heal(int* hp, int amount) {
-    if (*hp + amount <= 100) {
-        *hp += amount;
-    }
-}
-void printHp(const int* hp) {
-    std::cout << "HP posle = " << *hp << std::endl;
+void openChest(int* gold, int* chest) {
+    *gold += *chest;
+    *chest = 0;
 }
 
-bool isAlive(const int* hp) {
-    if (*hp > 0) {
-        std::cout << "player alive" << std::endl;
-        return true;
+void trapDamage(int* hp, int damage) {
+    *hp -= damage;
+    if (*hp < 0) {
+        *hp = 0;
     }
-    else {
-        std::cout << "player die, game over" << std::endl;
-    }
-    return false;
 }
 
+void swapChests(int* first, int* second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
 
-int main()
-{
-    std::cout << "player in dungeon" << std::endl;
+void printArray(const int* arr, int size) {
+    std::cout << "Chests: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+        if (i < size - 1) {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+int* findRichestChest(int* arr, int size) {
+    if (size <= 0) {
+        return nullptr;
+    }
+
+    int* richest = arr;
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > *richest) {
+            richest = &arr[i];
+        }
+    }
+
+    return richest;
+}
+
+int main() {
     int hp = 100;
-    std::cout << "HP = " << hp << std::endl;
-    std::cout << "goblin attack = 30" << std::endl;
-    takeDamage(&hp, 30);
-    printHp(&hp);
-    std::cout << "igrok piet zelie" << std::endl;
-    heal(&hp, 20);
-    printHp(&hp);
-    isAlive(&hp);
-}
+    int gold = 0;
+    int chests[5] = { 10, 0, 25, 50, 5 };
 
+    std::cout << "start HP:" << std::endl;
+    std::cout << "HP: " << hp << ", Gold: " << gold << std::endl;
+    printArray(chests, 5);
+    std::cout << std::endl;
+
+    openChest(&gold, &chests[0]);
+    std::cout << "chest 10 gold:" << std::endl;
+    std::cout << "HP: " << hp << ", Gold: " << gold << std::endl;
+    printArray(chests, 5);
+    std::cout << std::endl;
+
+    trapDamage(&hp, 15);
+    std::cout << "trap (-15 HP):" << std::endl;
+    std::cout << "HP: " << hp << ", Gold: " << gold << std::endl;
+    printArray(chests, 5);
+    std::cout << std::endl;
+
+    int* richest = findRichestChest(chests, 5);
+    std::cout << "most expensive chest have : " << *richest << " gold" << std::endl;
+    std::cout << std::endl;
+
+    swapChests(&chests[1], &chests[3]);
+    std::cout << "swap 2 and 4 chest:" << std::endl;
+    printArray(chests, 5);
+    std::cout << std::endl;
+
+    richest = findRichestChest(chests, 5);
+    openChest(&gold, richest);
+    std::cout << "open most expensive chest:" << std::endl;
+    std::cout << "HP: " << hp << ", Gold: " << gold << std::endl;
+    printArray(chests, 5);
+}
